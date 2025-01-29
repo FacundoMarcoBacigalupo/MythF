e42839759eebbd1d42f93b359c4fb627718a691a
Facundo <marcofacundolucas@gmail.com>
2025-01-28 21:21:41 -0300

    Fix build issues and adjust Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
+// Necesario para obtener la ruta del directorio actual
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Define "@" como alias para "src".
+    },
+  },
 })
