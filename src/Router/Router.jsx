1bf3926f55b6cd8bf745c89a21955ef203dccc4e
Facundo <marcofacundolucas@gmail.com>
2025-02-01 14:18:41 -0300

    fixx

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom" 
-import Navbar from '../components/navbar/navbar.jsx';
-import Footer from "../components/Footer/Footer.jsx"
-import Home from '../components/Home/Home.jsx';
-import Projects from '../components/Projects/Projects.jsx';
-import ServicesMarketing from '../components/ServicesMarketing/ServicesMarketing.jsx';
-import ServicesDesarrollo from '../components/ServicesServicesDesarrollo/ServicesDesarrollo.jsx';
-import Contact from '../components/Contact/Contact.jsx';
+import Navbar from "../components/Navbar/Navbar.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Home from "../components/Home/Home.jsx";
+import Projects from "../components/Projects/Projects.jsx";
+import ServicesMarketing from "../components/ServicesMarketing/ServicesMarketing.jsx";
+import ServicesDesarrollo from "../components/ServicesDesarrollo/ServicesDesarrollo.jsx"; //
+import Contact from "../components/Contact/Contact.jsx";
 
 const Router = () => {
     return (
